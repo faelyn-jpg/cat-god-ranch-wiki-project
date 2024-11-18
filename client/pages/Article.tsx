608183a4c235cd9194f28488bdd147f756c96fb4
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-11-18 17:18:41 +1300

    delete react-draft-wysiwyg

--- client/pages/Article.tsx
+++ client/pages/Article.tsx
@@ -1,11 +1,7 @@
-import { Editor } from 'react-draft-wysiwyg'
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
-
 function Article() {
   return (
     <>
       <h1>Article goes here!</h1>
-      <Editor />
     </>
   )
 }
